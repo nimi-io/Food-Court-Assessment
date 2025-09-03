569314d717fadc6a87d7058ba8350267cacb80de
nimi-io <ibukunoluwaodunsi@gmail.com>
2025-09-03 12:25:22 +0100

    Add global config module with validation to NestJS app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(
+    `🚀🚀🚀 - - - Application is running on: ${await app.getUrl()} - - - 🚀🚀🚀`,
+  );
 }
 bootstrap();
